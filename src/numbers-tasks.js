7f57ad8bc826daa62e59d1047b27ff29f54d065c
iZeevens <129598659+iZeevens@users.noreply.github.com>
2023-11-15 14:09:27 +0600

    feat: add toNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -233,8 +233,12 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  // eslint-disable-next-line no-restricted-globals
+  if (!isNaN(value)) {
+    return Number(value);
+  }
+  return def;
 }
 
 /**
